27dfd946766069099d640b7933b715191c875ac8
sakura745 <745879871@qq.com>
2021-09-08 20:49:37 +0800

    Sep 8-1

--- include/Linkedlist_palindrome.h
+++ include/Linkedlist_palindrome.h
@@ -15,8 +15,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-//对于给定的输出长度不确定时候。用数学归纳法来判断自己选择的方法是否正确。如Linkedlist长度分别为0，1，2，3，4
-//基本上可以确定是否试用了，因为再长的都同理
+//对于给定的输出长度不确定时候。用数学归纳法来判断自己选择的方法是否正确。如Linkedlist长度分别为0，1，2，3，4开始找规律
+//基本上可以确定是否适用了，因为再长的都同理
+//法一
 ////code代码为先用快慢指针找到中点位置，将后半部分push stack，在弹出stack与head比较
 class Solution {
 public:
@@ -26,7 +27,7 @@ public:
         ListNode *fast = head;
         ListNode *slow = head->next;
 
-        //&&左操作数的条件要比&&右操作数的条件先达到
+        //&&左操作数的条件要比&&右操作数的条件先达到，左操作数相当右操作数的一个if语句
         while(fast->next != nullptr && fast->next->next != nullptr){
             fast = fast->next->next;
             slow = slow->next;
@@ -46,7 +47,7 @@ public:
         return true;
     }
 };
-
+//法二：
 //反转列表而不是push stack pop stack
 class Solution {
 public:
@@ -69,15 +70,15 @@ public:
     }
 private:
     ListNode* reverseList(ListNode* head){
-        if(head->next == nullptr) return head;
+        if(head->next == nullptr || head == nullptr) return head;
         ListNode *pre = nullptr, *cur = head;
         while(cur != nullptr){
-            ListNode* tmp = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = tmp;
+            ListNode* tmp = cur->next;//断开cur与cur下一个node的link
+            cur->next = pre;//将cur指向pre反向link
+            pre = cur;//pre移动到cur位置
+            cur = tmp;//cur移动到tmp位置
         }
-        head->next = cur;
+        head->next = cur;//将反转的链表恢复，为了整个链表正常
         return pre;
     }
 };
