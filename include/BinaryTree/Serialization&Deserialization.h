65c8e12eda74f731055e2c204ffc8a0d8b18c767
sakura745 <745879871@qq.com>
2021-09-22 15:32:36 +0800

    Sep 22

--- include/BinaryTree/Serialization&Deserialization.h
+++ include/BinaryTree/Serialization&Deserialization.h
@@ -9,7 +9,7 @@ class Solution{
 public:
     string serialByPre(TreeNode* root){
         if(root == nullptr) return "#_";
-        string res = root->val + "_";
+        string res = to_string(root->val) + "_";
         res += serialByPre(root->left);
         res += serialByPre(root->right);
         return res;
@@ -18,11 +18,36 @@ public:
 
 class Solution{
 public:
-    TreeNode* deserialByPre(string preStr){
-
+    TreeNode* deserialPreString(string str){
+        string values = split(str, '_');
+        queue<string> queue;
+        for(int i = 0; i != values.size(); i++){
+            queue.push(values);
+        }
+        return deserial(queue);
     }
 private:
-    vector<string>
+    string split(string str, char del){
+        string res;
+        stringstream ss(str);
+        string s;
+        while(getline(ss, s, del)){
+            res += s;
+        }
+        return res;
+    }
+
+    TreeNode* deserial(queue<string> queue){
+        string value = queue.front();
+        queue.pop();
+        if(value == "#"){
+            return nullptr;
+        }
+        TreeNode* root = new TreeNode(stoi(value));
+        root->left = deserial(queue);
+        root->right = deserial(queue);
+        return root;
+    }
 };
 
 #endif //ALGRITHOM_SERIALIZATION_DESERIALIZATION_H
---
+++ include/Graph/README.md
@@ -0,0 +1,11 @@
+可以将图的问题转化为其他已知的结构的问题。
+
+```cpp
+class Graph{
+public:
+    unordered_map<int, Node> nodes;
+    unordered_set<Edge> deges;
+    Gra
+    
+};
+```
