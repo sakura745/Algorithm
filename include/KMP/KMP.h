2a2b2f8a46f0161819f4ea57825e7c9a358aeefe
CarBO <745879871@qq.com>
2021-09-24 23:34:29 +0800

    Sep 24 _Legion

--- include/KMP/KMP.h
+++ include/KMP/KMP.h
@@ -9,24 +9,22 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         int h = haystack.size(), n = needle.size();
-        if(h == 0 && n == 0) return 0;
-        else if ( h != 0 && n == 0) return 0;
-        else if( h == 0 || n == 0 || h < n) return -1;
+        if(n == 0) return 0;
+        if(h < n) return -1;
         int i = 0, j = 0;
         int *next = nextVector(needle);
         //j跳出while表示已经配出相同的了，才会导致j++到出界
         //不是因为j跳出的while，说明没有找到
         while(i < h && j < n){
-            if(haystack[i] == needle[j]){
+            if(haystack[i] == needle[j]) {
                 i++;
                 j++;
-            } else if(next[j] == -1){//j == 0
-                i++;
-            } else {
-                j = next[j];
             }
+            //next[j] == -1
+            else if(j == 0) i++;
+            else  j = next[j];
         }
-        return j == n ? i - j : -1;//i1越界了，；i2越界了表示没有找到，返回-1
+        return j == n ? i - j : -1;//i越界了；j越界了表示没有找到，返回-1
     }
 private:
     int* nextVector(string s){
@@ -34,16 +32,12 @@ private:
         res[0] = -1;
         if(s.size() == 1) return res;
         res[1] = 0;
-        int i = 2;
-        int cn = 0;//哪个位置的字符和i-1的字符比较
+        int i = 2, j = 0;//哪个位置的字符和i-1的字符比较
         while(i < s.size()){
-            if(s[i - 1] == s[cn]){
-                res[i++] = ++cn;
-            } else if (cn > 0){//cn往前跳
-                cn = res[cn];
-            } else {
-                res[i++] = 0;
-            }
+            if(s[i - 1] == s[j])res[i++] = ++j;
+                //cn往前跳
+            else if (j > 0){j = res[j];
+            else res[i++] = 0;
         }
         return res;
     }
