9202464f4af4f2e11fc972698d05b70d3549b00b
sakura745 <745879871@qq.com>
2021-09-26 22:04:42 +0800

    Sep 24

--- include/Manacher/Manacher.h
+++ include/Manacher/Manacher.h
@@ -9,18 +9,18 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         string str = expandString(s);
-        int pArray[str.size()], C = -1, R = -1, res = INT_MIN;
+        int pr[str.size()], C = -1, R = -1, res = INT_MIN;
         for(int i = 0; i < str.size(); i++){
-            pArray[i] = R > i ? min(pArray[2 * C - i], R - i) : 1;
-            while(i + pArray[i] < str.size() && i - pArray[i] > -1){
-                if(str[i + pArray[i]] == str[i - pArray[i]]) pArray[i]++;
+            pr[i] = R > i ? min(pr[2 * C - i], R - i) : 1;
+            while(i + pr[i] < str.size() && i - pr[i] > -1){
+                if(str[i + pr[i]] == str[i - pr[i]]) pr[i]++;
                 else break;
             }
-            if(i + pArray[i] > R){
-                R = i + pArray[i];
+            if(i + pr[i] > R){
+                R = i + pr[i];
                 C = i;
             }
-            res = max(res, pArray[i]);
+            res = max(res, pr[i]);
         }
         return res - 1;
     }
